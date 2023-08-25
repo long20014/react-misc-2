886a3fdca88477942ec341e409c14edebba7de4d
long.truong <long.truong@linecorp.com>
2023-08-25 10:13:59 +0700

    add enemy simulator

--- src/components/GameDamageDemo/GameDamageDemo.tsx
+++ src/components/GameDamageDemo/GameDamageDemo.tsx
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { getRandomArbitrary } from './util';
 
+const MAX_HP = 5000;
+
+type DamageObj = {
+  damage: number;
+  isCritical: boolean;
+};
+
 const GameDamageDemo: React.FC<any> = (props) => {
-  const [damage, setDamage] = useState(0);
-  const [damageList, setDamageList] = useState<number[]>([]);
+  const [damageList, setDamageList] = useState<DamageObj[]>([]);
+  const [damageListEnemy, setDamageListEnemy] = useState<DamageObj[]>([]);
+  const [HP, setHP] = useState(MAX_HP);
+  const [enemyHP, setEnemyHP] = useState(MAX_HP);
+  const defeated = useRef(false);
+  const enemyDefeated = useRef(false);
+
+  useEffect(() => {
+    if (HP < 0 && !defeated.current) {
+      defeated.current = true;
+    }
+    if (enemyHP < 0 && !enemyDefeated.current) {
+      enemyDefeated.current = true;
+    }
+  }, [HP, enemyHP, defeated, enemyDefeated]);
 
   const baseAttack = 100;
   const criticalRate = 0.5;
@@ -11,46 +31,120 @@ const GameDamageDemo: React.FC<any> = (props) => {
   const attackIntensity = 1000;
   const skillDamageAmplify = 2;
   const skillBaseDamage = 300;
+  const accuracy = 1;
 
   const enemyDefense = 500;
   const enemyCritResist = 0.2;
   const enemyCritDmgResist = 0.5;
+  const enemyEvasion = 0.3;
 
-  const calculateDamage = () => {
+  const timeout = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const calculateDamageObj = () => {
     const isCritical = getRandomArbitrary(0, 1, 2) <= criticalRate - enemyCritResist;
     const criticalAmplify = isCritical ? 1 + criticalDamage - enemyCritDmgResist : 1;
     const damageDelta = getRandomArbitrary(0.8, 1.2, 2);
-    const damageOutput =
-      (baseAttack * skillDamageAmplify + skillBaseDamage) *
-      criticalAmplify *
-      damageDelta *
-      ((attackIntensity - enemyDefense) / enemyDefense);
-    return damageOutput;
+    const isHit = getRandomArbitrary(0, 1, 2) <= accuracy - enemyEvasion;
+    const damageOutput = isHit
+      ? (baseAttack * skillDamageAmplify + skillBaseDamage) *
+        criticalAmplify *
+        damageDelta *
+        ((attackIntensity - enemyDefense) / enemyDefense)
+      : 0;
+    return { damage: damageOutput, isCritical };
   };
 
-  const calculateDamageList = () => {
-    const arr: number[] = [];
+  const calculateDamageList = async () => {
+    const arr: any[] = [];
     for (let i = 0; i < 20; i++) {
-      arr.push(calculateDamage());
+      if (defeated.current || enemyDefeated.current) {
+        return arr;
+      }
+      console.log(i);
+      await timeout(500);
+      const damageObj = calculateDamageObj();
+      arr.push(damageObj);
+      setDamageList([...arr]);
+      setEnemyHP((enemyHP) => enemyHP - damageObj.damage);
+      if (HP < 0 && !defeated) {
+      }
     }
     return arr;
   };
 
-  const getNewDamageList = () => {
-    setDamageList(calculateDamageList());
+  const calculateDamageListEnemy = async () => {
+    const arr: any[] = [];
+    for (let i = 0; i < 20; i++) {
+      if (defeated.current || enemyDefeated.current) {
+        return arr;
+      }
+      await timeout(300);
+      const damageObj = calculateDamageObj();
+      arr.push(damageObj);
+      setDamageListEnemy([...arr]);
+      setHP((HP) => HP - damageObj.damage);
+    }
+    return arr;
+  };
+
+  const startFight = async () => {
+    calculateDamageList();
+    calculateDamageListEnemy();
+  };
+
+  const resetFight = () => {
+    setDamageList([]);
+    setDamageListEnemy([]);
+    setHP(MAX_HP);
+    setEnemyHP(MAX_HP);
+    defeated.current = false;
+    enemyDefeated.current = false;
   };
 
   return (
     <div style={{ display: 'block', justifyContent: 'center' }}>
       <h2>Damage Calculate Demo</h2>
-      <button onClick={getNewDamageList}>Calculate Damage</button>
+      <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '400px' }}>
+        <button onClick={startFight}>Start Fight</button>
+        <button onClick={resetFight}>Reset Fight</button>
+      </div>
+
       <br />
       <br />
-      <ul>
-        {damageList.map((damage, i) => {
-          return <li key={i}>{damage}</li>;
-        })}
-      </ul>
+      <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '400px' }}>
+        <h3>{HP}</h3>
+        <h3>{enemyHP}</h3>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '400px' }}>
+        <div>
+          <ul>
+            {damageList.map((item, i) => {
+              const { damage, isCritical } = item;
+              return (
+                <li key={i} style={{ color: isCritical ? 'red' : 'black' }}>
+                  {damage}
+                </li>
+              );
+            })}
+          </ul>
+          <div>{defeated.current ? 'Defeated' : 'Alive'}</div>
+        </div>
+        <div>
+          <ul>
+            {damageListEnemy.map((item, i) => {
+              const { damage, isCritical } = item;
+              return (
+                <li key={i} style={{ color: isCritical ? 'red' : 'black' }}>
+                  {damage}
+                </li>
+              );
+            })}
+          </ul>
+          <div>{enemyDefeated.current ? 'Enemy Defeated' : 'Enemy Alive'}</div>
+        </div>
+      </div>
     </div>
   );
 };
